e835677acf4bc438b7a84d27a75a185ab164469a
Salton Massally <salton.massally@gmail.com>
2024-06-02 16:58:15 -0400

    add skip_wait_on_job_termination

--- modules/dwh-vpc-sc/variables.tf
+++ modules/dwh-vpc-sc/variables.tf
@@ -110,8 +110,20 @@ variable "access_level_regions" {
 variable "egress_policies" {
   description = "A list of all [egress policies](https://cloud.google.com/vpc-service-controls/docs/ingress-egress-rules#egress-rules-reference), each list object has a `from` and `to` value that describes egress_from and egress_to. See also [secure data exchange](https://cloud.google.com/vpc-service-controls/docs/secure-data-exchange#allow_access_to_a_google_cloud_resource_outside_the_perimeter)."
   type = list(object({
-    from = any
-    to   = any
+    from = object({
+      identity_type = string
+      identities    = optional(list(string))
+      sources = object({
+        access_level = optional(list(string))
+        resource     = optional(list(string))
+      })
+    })
+    to = object({
+      resources = list(string)
+      operations = map(object({
+        methods = list(string)
+      }))
+    })
   }))
   default = []
 }
@@ -119,8 +131,20 @@ variable "egress_policies" {
 variable "ingress_policies" {
   description = "A list of all [ingress policies](https://cloud.google.com/vpc-service-controls/docs/ingress-egress-rules#ingress-rules-reference), each list object has a `from` and `to` value that describes ingress_from and ingress_to.\n\nExample: `[{ from={ sources={ resources=[], access_levels=[] }, identities=[], identity_type=\"ID_TYPE\" }, to={ resources=[], operations={ \"SRV_NAME\"={ OP_TYPE=[] }}}}]`\n\nValid Values:\n`ID_TYPE` = `null` or `IDENTITY_TYPE_UNSPECIFIED` (only allow indentities from list); `ANY_IDENTITY`; `ANY_USER_ACCOUNT`; `ANY_SERVICE_ACCOUNT`\n`SRV_NAME` = \"`*`\" (allow all services) or [Specific Services](https://cloud.google.com/vpc-service-controls/docs/supported-products#supported_products)\n`OP_TYPE` = [methods](https://cloud.google.com/vpc-service-controls/docs/supported-method-restrictions) or [permissions](https://cloud.google.com/vpc-service-controls/docs/supported-method-restrictions)."
   type = list(object({
-    from = any
-    to   = any
+    from = object({
+      identity_type = string
+      identities    = optional(list(string))
+      sources = object({
+        access_level = optional(list(string))
+        resource     = optional(list(string))
+      })
+    })
+    to = object({
+      resources = list(string)
+      operations = map(object({
+        methods = list(string)
+      }))
+    })
   }))
   default = []
 }
